bebae3149ed0cb1ea364065125a6b476d8c27cd7
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-08-15 18:43:58 +0200

    Aktualisieren von setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ pkg install tmux -y
 pkg install wget -y
 pd install ubuntu
 pd sh ubuntu -- sh -c "yes | apt update && yes | apt upgrade"
-pd sh ubuntu -- sh -c "yes | sudo apt install" software-properties-common"
 pd sh ubuntu -- sh -c "apt install sudo nano -y"
 pd sh ubuntu -- sh -c 'useradd -m -s /bin/bash euphoriys && echo "euphoriys:euphoriys" | chpasswd'
 pd sh ubuntu -- sh -c 'usermod -aG sudo euphoriys && echo "euphoriys ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/euphoriys'
