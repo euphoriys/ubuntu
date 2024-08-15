22d3392b97e65fc894399161f4c6391ab0f32126
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-08-15 13:19:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ pd sh ubuntu -- sh -c "apt install sudo nano -y"
 pd sh ubuntu -- sh -c 'useradd -m -s /bin/bash euphoriys && echo "euphoriys:euphoriys" | chpasswd'
 pd sh ubuntu -- sh -c 'usermod -aG sudo euphoriys && echo "euphoriys ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/euphoriys'
 pd sh ubuntu -- sh -c "sudo apt install xfce4"
+wget https://raw.githubusercontent.com/euphoriys/ubuntu/main/ubuntu && mv ubuntu ../usr/bin/ubuntu && chmod +x ../usr/bin/ubuntu
