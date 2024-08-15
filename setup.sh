e5aec0158507c7d4f3e8fb8a70006b05e07a5d15
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-08-15 05:29:04 +0200

    Aktualisieren von setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ yes | pkg up
 pkg install proot-distro -y && pd install ubuntu
 pd sh ubuntu -- sh -c "yes | apt update && yes | apt upgrade"
 pd sh ubuntu -- sh -c "apt install sudo nano -y"
-pd sh ubuntu -- sh -c "useradd -m -s /bin/bash onuralp && echo 'onuralp:onuralp' | chpasswd
+pd sh ubuntu -- sh -c 'useradd -m -s /bin/bash onuralp && echo "onuralp:onuralp" | chpasswd'
 pd sh ubuntu -- sh -c 'usermod -aG sudo onuralp && echo "onuralp ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/onuralp'
