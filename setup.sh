291ff10f8d1f6610c3ccd130d705b9ef1fa3f963
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-08-15 13:52:59 +0200

    Aktualisieren von setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 yes | pkg up
 pkg install proot-distro x11-repo virglrenderer-android wget -y
-pkg install termux-x11-nightly
+pkg install termux-x11-nightly -y
 pkg install pulseaudio -y 
 pd install ubuntu && pd sh ubuntu -- sh -c "yes | apt update && yes | apt upgrade"
 pd sh ubuntu -- sh -c "apt install sudo nano -y"
