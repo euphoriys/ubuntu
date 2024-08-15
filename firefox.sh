3ffb50d8cffc87e02c956e2b701edf50520571ae
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-08-15 18:47:21 +0200

    Aktualisieren von firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-pd sh ubuntu -- sh -c "sudo apt install software-properties-common"
-pd sh ubuntu -- sh -c "sudo add-apt-repository ppa:mozillateam/ppa"
-pd sh ubuntu -- sh -c '
-echo "
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:mozillateam/ppa
+echo '
 Package: *
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
 Package: firefox
 Pin: version 1:1snap*
 Pin-Priority: -1
-" | sudo tee /etc/apt/preferences.d/mozilla-firefox'
-pd sh ubuntu -- sh -c "sudo apt install firefox"
-pd sh ubuntu -- sh -c 'echo "Unattended-Upgrade::Allowed-Origins:: \"LP-PPA-mozillateam:${distro_codename}\";" | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox'
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+sudo apt install firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
