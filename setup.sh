03025bf1320add7986b9c91b07be9ceff4ea7f6b
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-08-15 14:41:29 +0200

    Aktualisieren von setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 yes | pkg up
-pkg install proot-distro x11-repo termux-x11-nightly virglrenderer-android pulseaudio tmux wget -y
+pkg install proot-distro -y
+pkg install x11-repo -y
+pkg install termux-x11-nightly -y
+pkg install virglrenderer-android -y
+pkg install pulseaudio -y
+pkg install tmux -y
+pkg install wget -y
 pd install ubuntu && pd sh ubuntu -- sh -c "yes | apt update && yes | apt upgrade"
 pd sh ubuntu -- sh -c "apt install sudo nano -y"
 pd sh ubuntu -- sh -c 'useradd -m -s /bin/bash euphoriys && echo "euphoriys:euphoriys" | chpasswd'
