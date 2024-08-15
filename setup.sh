1186d5eb8423a69288a3c42d8393d344ec84dd83
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-08-15 13:17:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 yes | pkg up
-pkg install proot-distro -y && pd install ubuntu
-pd sh ubuntu -- sh -c "yes | apt update && yes | apt upgrade"
+pkg install proot-distro x11-repo termux-x11-nightly pulseaudio virglrenderer-android wget -y 
+pd install ubuntu && pd sh ubuntu -- sh -c "yes | apt update && yes | apt upgrade"
 pd sh ubuntu -- sh -c "apt install sudo nano -y"
-pd sh ubuntu -- sh -c 'useradd -m -s /bin/bash onuralp && echo "onuralp:onuralp" | chpasswd'
-pd sh ubuntu -- sh -c 'usermod -aG sudo onuralp && echo "onuralp ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/onuralp'
+pd sh ubuntu -- sh -c 'useradd -m -s /bin/bash euphoriys && echo "euphoriys:euphoriys" | chpasswd'
+pd sh ubuntu -- sh -c 'usermod -aG sudo euphoriys && echo "euphoriys ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/euphoriys'
+pd sh ubuntu -- sh -c "sudo apt install xfce4"
